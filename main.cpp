c4d0d4cd8921fcb9a48337220a2236e96376f8a7
Jeonsw <fpdlsnsqlc3@naver.com>
2024-05-08 16:32:35 +0900

    부모 나이 질문지 추가

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int main()
 {
 	int _age = 0;
+	int _motherage = 0;
 
 	cout << "Hello World" << endl;
 	cout << "what u r age?";
 	cin >> _age;
-	cout << "u r age is " << _age;
+	cout << "u r age is " << _age << endl;
+	cout << "what u r Mother age?";
+	cin >> _motherage;
 
 	return 0;
 }
