eefad2169712ba7af2afaa45525d5febe3ab38d7
Jeonsw <fpdlsnsqlc3@naver.com>
2024-05-08 17:34:05 +0900

    아버지 나이 질문 및 답변 추가

--- main.cpp
+++ main.cpp
@@ -5,14 +5,18 @@ int main()
 {
 	int _age = 0;
 	int _motherage = 0;
+	int _fatherage = 0;
 
 	cout << "Hello World" << endl;
-	cout << "what u r age?";
+	cout << "what u r age? ";
 	cin >> _age;
 	cout << "u r age is " << _age << endl;
-	cout << "what u r Mother age?";
+	cout << "what u r Mother age? ";
 	cin >> _motherage;
-	cout << "u r Mother age is " << _motherage;
+	cout << "u r Mother age is " << _motherage << endl;
+	cout << "what u r father age? ";
+	cin >> _fatherage;
+	cout << "u r father age is " << _fatherage << endl;
 
 	return 0;
 }
